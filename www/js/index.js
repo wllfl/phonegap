c043596735f5c22a14623375506ee02b93345060
William <wllfl@ig.com.br>
2014-11-24 22:21:58 -0200

    Primeiro Commit

---
+++ www/js/index.js
@@ -0,0 +1,36 @@
+
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        var option;
+
+        option = {
+            timeout: 10000,
+            enableHighAccuracy: true 
+        }
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, option);
+    }
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          + position.timestamp                    + '<br />';
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+        var element = document.getElementById('geolocation');
+        element.innerHTML('fgdfgdf');
+    }
